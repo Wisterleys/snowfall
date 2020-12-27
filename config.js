4efb2945a72b4930fa66ff53e3a4bbd43f76b4ef
Wisterleys Sousa <71773504+Wisterleys@users.noreply.github.com>
2020-12-27 20:59:48 -0300

    conf direction ++

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ const CONFIG={
     choice:"neve",//Only snow has been mounted so far
     intervalCreate:1500,// Milliseconds
     creationPause:10000,// Milliseconds
-    removeSnowDuration:2000,// Milliseconds
+    removeSnowDuration:8000,// Milliseconds
     windConf:{
         wind:true,
         windDirection:-1,// 1 = left || -1 = right
-        windSpeed:2
+        windSpeed:3
     }
 }
 //-----------------------------------------------------
